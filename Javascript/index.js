e33363212ef544d29047e3d248b7bd5fac95081e
unknown <ebenezer.ansere@amalitech.org>
2021-06-25 16:55:55 +0000

    final commit

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,8 +1,14 @@
+let itemsCount = [];
+
+
 window.addEventListener('load', (event) => {
     renderDetails();
+    showproductCount();
+    showCategory();
 });
 const renderDetails = () => {
     let products = JSON.parse(localStorage.getItem("products"));
+    let counter = 0;
 
     let tableBody = document.querySelector("tbody");
 
@@ -41,7 +47,7 @@ const renderDetails = () => {
 
 
 
-        let counter = 0;
+
         let itemsInStock = JSON.parse(localStorage.getItem("products"));
         console.log(itemsInStock)
         itemsInStock.forEach((item) => {
@@ -61,5 +67,62 @@ const renderDetails = () => {
         console.log(itemsInStock);
         localStorage.setItem("products", JSON.stringify(itemsInStock))
     }
+
+
+
+
+
+
+}
+
+const showproductCount = () => {
+
+    let itemsInStock = JSON.parse(localStorage.getItem("products"));
+
+    let itemsCount = itemsInStock.length;
+    const categoryDiv = document.querySelector(".group-container");
+    categoryDiv.children[0].children[0].children[0].innerText = itemsCount;
+    categoryDiv.children[2].children[0].children[0].innerText = itemsCount;
+}
+
+
+const showCategory = () => {
+    const categoryDiv = document.querySelector(".group-container");
+    let itemsInStock = JSON.parse(localStorage.getItem("products"));
+
+
+    // console.log(itemsInStock);
+    itemsInStock.forEach((item) => {
+        console.log(item.category);
+        if (itemsCount.length < 0) {
+
+            alert(item.category);
+            itemsCount.push(item.category);
+        } else {
+
+            if (checkCategory(item.category) == null) {
+                itemsCount.push(item.category);
+            }
+        }
+    })
+
+    console.log(categoryDiv.children[0].children[0].children[0].innerText);
+
+
+    categoryDiv.children[1].children[0].children[0].innerText = itemsCount.length;
+}
+
+
+
+const checkCategory = (category) => {
+    itemsCount.forEach((item, index) => {
+        console.log(item)
+        if (item === category) {
+            return category;
+        }
+    })
+
+
+    return null;
 
 }
